f05b6bc08472aebff1807c5ebd670622ae87657e
key5771 <ksj57715@hanmail.net>
2020-09-23 17:35:54 +0900

    사진촬영 및 앨범저장 완료

--- FullScreenCamera/FullScreenCamera/VC/CameraController.swift
+++ FullScreenCamera/FullScreenCamera/VC/CameraController.swift
@@ -27,6 +27,8 @@ class CameraController: UIViewController {
     // preview
     var previewLayer: AVCaptureVideoPreviewLayer?
     
+    var photoCaptureCompletionBlock: ((UIImage?, Error?) -> Void)?
+    
     
     // Preview display
     func displayPreview(on view: UIView) throws {
@@ -62,79 +64,117 @@ extension CameraController {
 
 // MARK: - Camera 동작 기능
 extension CameraController {
+//    func prepare(completionHandler: @escaping (Error?) -> Void) {
+//        self.captureSession = AVCaptureSession()
+//        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .unspecified) else {
+//            fatalError("no dual camera")
+//        }
+//
+//        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice), ((self.captureSession?.canAddInput(videoDeviceInput)) != nil) else {
+//            fatalError("Can't add video input")
+//        }
+//
+//        self.captureSession?.beginConfiguration()
+//        self.captureSession?.addInput(videoDeviceInput)
+//
+//        let photoOutput = AVCapturePhotoOutput()
+//        photoOutput.isDepthDataDeliveryEnabled = photoOutput.isDepthDataDeliverySupported
+//
+//        guard ((self.captureSession?.canAddOutput(photoOutput)) != nil) else {
+//            fatalError("Can't add photo output")
+//        }
+//
+//        self.captureSession?.addOutput(photoOutput)
+//        self.captureSession?.sessionPreset = .photo
+//        self.captureSession?.commitConfiguration()
+//
+//        let photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.hevc])
+//        photoSettings.isDepthDataDeliveryEnabled = photoOutput.isDepthDataDeliverySupported
+//    }
+    
     func prepare(completionHandler: @escaping (Error?) -> Void) {
         // #1 : Creating a capture session
         func createCaptureSession() {
             self.captureSession = AVCaptureSession()
         }
-        
+
         // #2 : Obtaining and configuring the necessary capture devices
         func configureCaptureDevices() throws {
-            let session = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .unspecified)
-            guard let cameras = (session?.constituentDevices.compactMap { $0 }), !cameras.isEmpty else {
-                throw CameraControllerError.noCamerasAvailable
-            }
+//            let session = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .unspecified)
+//            guard let cameras = (session?.constituentDevices.compactMap { $0 }), !cameras.isEmpty else {
+//                throw CameraControllerError.noCamerasAvailable
+//            }
+//
+//            for camera in cameras {
+//                if camera.position == .front {
+//                    self.frontCamera = camera
+//                }
+//
+//                if camera.position == .back {
+//                    self.rearCamera = camera
+//
+//                    try camera.lockForConfiguration()
+//                    camera.focusMode = .continuousAutoFocus
+//                    camera.unlockForConfiguration()
+//                }
+//            }
             
-            for camera in cameras {
-                if camera.position == .front {
-                    self.frontCamera = camera
-                }
-                
-                if camera.position == .back {
-                    self.rearCamera = camera
-                    
-                    try camera.lockForConfiguration()
-                    camera.focusMode = .continuousAutoFocus
-                    camera.unlockForConfiguration()
-                }
+            if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+                self.rearCamera = device
+            } else if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+                self.rearCamera = device
+            } else {
+                fatalError("Missing expected back camera device")
             }
         }
-        
+
         // #3 : Creating inputs using the capture devices
         func configureDeviceInputs() throws {
             guard let captureSession = self.captureSession else {
                 throw CameraControllerError.captureSessionIsMissing
             }
-            
+
             if let rearCamera = self.rearCamera {
                 self.rearCameraInput = try AVCaptureDeviceInput(device: rearCamera)
-                
+
                 if captureSession.canAddInput(self.rearCameraInput!) {
                     captureSession.addInput(self.rearCameraInput!)
                 }
-                
+
                 self.currentCameraPosition = .rear
-            } else if let frontCamera = self.frontCamera {
-                self.frontCameraInput = try AVCaptureDeviceInput(device: frontCamera)
-                
-                if captureSession.canAddInput(self.frontCameraInput!) {
-                    captureSession.addInput(self.frontCameraInput!)
-                } else {
-                    throw CameraControllerError.inputsAreInvalid
-                }
-                
-                self.currentCameraPosition = .front
-            } else {
+            }
+//            else if let frontCamera = self.frontCamera {
+//                self.frontCameraInput = try AVCaptureDeviceInput(device: frontCamera)
+//
+//                if captureSession.canAddInput(self.frontCameraInput!) {
+//                    captureSession.addInput(self.frontCameraInput!)
+//                } else {
+//                    throw CameraControllerError.inputsAreInvalid
+//                }
+//
+//                self.currentCameraPosition = .front
+//            }
+            else {
                 throw CameraControllerError.noCamerasAvailable
             }
         }
-        
+
         // #4 : Configuring a photo output object to process captured images
         func configurePhotoOutput() throws {
             guard let captureSession = self.captureSession else {
                 throw CameraControllerError.captureSessionIsMissing
             }
-            
+
             self.photoOutput = AVCapturePhotoOutput()
             self.photoOutput!.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
-            
+
             if captureSession.canAddOutput(self.photoOutput!) {
                 captureSession.addOutput(self.photoOutput!)
             }
-            
+
             captureSession.startRunning()
         }
-        
+
         DispatchQueue(label: "prepare").async {
             do {
                 createCaptureSession()
@@ -145,13 +185,46 @@ extension CameraController {
                 DispatchQueue.main.async {
                     completionHandler(error)
                 }
-                
+
                 return
             }
-            
+
             DispatchQueue.main.async {
                 completionHandler(nil)
             }
+        }
+    }
+    
+    func captureImage(completion: @escaping (UIImage?, Error?) -> Void) {
+        guard let captureSession = captureSession, captureSession.isRunning else {
+            completion(nil, CameraControllerError.captureSessionIsMissing)
+            return
+        }
+        
+        let settings = AVCapturePhotoSettings()
+        self.photoOutput?.capturePhoto(with: settings, delegate: self)
+        self.photoCaptureCompletionBlock = completion
+    }
+}
+
+extension CameraController: AVCapturePhotoCaptureDelegate {
+    public func photoOutput(_ captureOutput: AVCapturePhotoOutput,
+                            didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?,
+                            previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?,
+                            resolvedSettings: AVCaptureResolvedPhotoSettings,
+                            bracketSettings: AVCaptureBracketedStillImageSettings?,
+                            error: Swift.Error?) {
+        
+        if let error = error {
+            self.photoCaptureCompletionBlock?(nil, error)
+        } else if let buffer = photoSampleBuffer, let data = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: buffer, previewPhotoSampleBuffer: nil),
+                let image = UIImage(data: data) {
+            
+            self.photoCaptureCompletionBlock?(image, nil)
+        }
+        
+        else {
+            self.photoCaptureCompletionBlock?(nil, CameraControllerError.unknown)
         }
     }
 }
--- FullScreenCamera/FullScreenCamera/VC/ViewController.swift
+++ FullScreenCamera/FullScreenCamera/VC/ViewController.swift
@@ -6,13 +6,62 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
-
+    let cameraController = CameraController()
+    
+    @IBOutlet fileprivate var captureButton: UIButton!
+    
+    @IBOutlet fileprivate var capturePreviewView: UIView!
+    
+    @IBOutlet fileprivate var photoModeButton: UIButton!
+//    @IBOutlet fileprivate var toggleCameraButton: UIButton!
+//    @IBOutlet fileprivate var tollgeFlashButton: UIButton!
+    
+    @IBOutlet fileprivate var videoModeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        func configureCameraController() {
+            cameraController.prepare { (error) in
+                if let error = error {
+                    print(error)
+                }
+
+                try? self.cameraController.displayPreview(on: self.capturePreviewView)
+            }
+        }
+
+        func styleCaptureButton() {
+            captureButton.layer.borderWidth = 2
+            captureButton.layer.borderColor = UIColor.black.cgColor
+
+            captureButton.layer.cornerRadius = min(captureButton.frame.width, captureButton.frame.height) / 2
+
+            photoModeButton.layer.borderWidth = 0
+            photoModeButton.layer.borderColor = UIColor.black.cgColor
+
+            videoModeButton.layer.borderWidth = 0
+            videoModeButton.layer.borderColor = UIColor.black.cgColor
+        }
+
+        styleCaptureButton()
+        configureCameraController()
+    }
+    
+    @IBAction func captureImage(_ sender: Any) {
+        cameraController.captureImage { (image, error) in
+            guard let image = image else {
+                print(error ?? "Image capture error")
+                return
+            }
+            
+            try? PHPhotoLibrary.shared().performChangesAndWait {
+                PHAssetChangeRequest.creationRequestForAsset(from: image)
+            }
+        }
     }
     
 
